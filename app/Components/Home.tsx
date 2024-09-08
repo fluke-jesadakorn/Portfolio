faaa4b1acf8ae2837b13a08f8c0d666f2e122a43
jesadakorn.k <jesadakorn.k@finstable.co.th>
2024-09-08 19:09:34 +0700

    deploy last version website

--- app/Components/Home.tsx
+++ app/Components/Home.tsx
@@ -1,55 +1,49 @@
 "use client";
-
 import {
   Layout,
   Typography,
   Button,
-  Card,
   Row,
   Col,
   Avatar,
   Space,
   Timeline,
-  Form,
-  Input,
 } from "antd";
 import Link from "next/link";
 
 const { Content } = Layout;
 const { Title, Text, Paragraph } = Typography;
 
-const mockupWorks = [
-  { title: "Designing Dashboards", year: "2020", category: "Dashboard" },
-  {
-    title: "Vibrant Portraits of 2020",
-    year: "2018",
-    category: "Illustration",
-  },
-  { title: "36 Days of Malayalam type", year: "2018", category: "Typography" },
-];
-
 const workExperiences = [
   {
-    title: "Freelance Website and Mobile Fullstack Developer",
-    company: "Freelance",
-    period: "2018-2020",
-    content: `Develop website and mobile application for customer.`,
+    title: "IT and Web and Mobile Developer",
+    company: "IMT GROUP COMPANY LIMITED",
+    period: "March 2021 - January 2022",
+    content: `Spearheaded the development of department-specific web and mobile applications aligned with organizational goals.
+              Established IT department resource planning initiatives to accommodate technological advancement and scalability.
+              Recommended tools and technologies to augment operational efficiency and productivity.
+              Managed workflow processes, adopting best practices for enhanced team coordination and project delivery.`,
   },
   {
-    title: "Startup Co-Founder",
-    company: "Virtual Assistance Project",
-    period: "2019-2020",
-    content: `Create application for virtual assistance.`,
+    title: "Head Engineer (Bitcoin Miner Machine)",
+    company: "TOKEN MINER COMPANY LIMITED",
+    period: "January 2022 - August 2024",
+    content: `Lead cross-departmental collaboration to craft comprehensive feasibility studies for strategic investment decisions.
+              Develop and implement processes for the procurement and maintenance of mining equipment.
+              Design and enforce Standard Operating Procedures (SOP) to streamline staff operations.
+              Analyze market trends to forecast cryptocurrency selling points and facilitate timely equipment upgrades.
+              Architect an organizational structure that optimizes management and team efficiency.
+              Orchestrate the design of facility layouts, ensuring operational functionality and safety compliance.
+              Drive disaster prevention initiatives, mitigating risks associated with fire and electrical loads.
+              Liaise with external service providers to enhance operational capabilities and performance.`,
   },
   {
-    title: "Head Engineer",
-    company: "Cryptocurrency Mining Farm Project",
-    period: "2020-Present",
-    content: `In my current role as Head Engineer overseeing the creation of a cryptocurrency mining farm,
-      I have demonstrated my capability to coordinate between financial and engineering departments to create comprehensive feasibility studies.
-      I am adept at managing complex budgets and working collaboratively with executive, finance, and compliance teams to plan and execute operations.
-      My experience includes designing and implementing ASIC mining rooms, working with third-party contractors,
-      and ensuring the highest standards of quality through rigorous quality control.`,
+    title: "Senior Developer (Backend and Frontend)",
+    company: "FINSTABLE COMPANY LIMITED",
+    period: "January 2022 - Present",
+    content: `Applying for a position focused on developing website applications, including both backend and frontend, based on customer requirements.
+              Core expertise in Customer Relationship Management (CRM) systems.
+              Experienced in implementing role-based approval workflows within applications.`,
   },
 ];
 
@@ -59,14 +53,7 @@ export default function Home() {
       <Space direction="vertical" size="large" style={{ display: "flex" }}>
         <Row align="middle" justify="space-between">
           <Col xs={24} sm={24} md={16} lg={18} xl={18}>
-            <Title>Hi, I am Jesadakorn</Title>
-            <Title level={4}>Head Engineer</Title>
-            <Paragraph>
-              Driving the engineering of a cutting-edge cryptocurrency mining
-              farm, I meld financial acuity with technical prowess to optimize
-              operations and quality control.
-            </Paragraph>
-            <Link href="/Resume.pdf"><Button type="primary">Download Resume</Button></Link>
+            <Title>Hi, I am Jesadakorn Kirtnu</Title>
           </Col>
           <Col xs={24} sm={24} md={8} lg={6} xl={6}>
             <Avatar size={120} src="/fluke.jpeg" style={{ float: "right" }} />
@@ -74,21 +61,10 @@ export default function Home() {
         </Row>
 
         <Title level={2} style={{ marginTop: "48px" }}>
-          Work Experience
+          Professional Experience
         </Title>
         <Row gutter={24}>
           <Col span={24}>
-            {/* <Timeline>
-                    {workExperiences.map((experience, index) => (
-                      <Timeline key={index}>
-                        <Title level={4}>{experience.title}</Title>
-                        <Title level={5}>{experience.company}</Title>
-                        <Text>{experience.period}</Text>
-                        <Paragraph>{experience.content}</Paragraph>
-                      </Timeline>
-                    ))}
-                  </Timeline> */}
-
             <Timeline
               items={workExperiences.map((experience, index) => {
                 return {
@@ -105,23 +81,6 @@ export default function Home() {
               })}
             />
           </Col>
-        </Row>
-
-        <Title level={3}>Featured works</Title>
-        <Row gutter={16}>
-          {mockupWorks.map((work) => (
-            <Col span={8} key={work.title}>
-              <Card
-                hoverable
-                cover={<img alt={work.title} src="/fluke.jpeg" />}
-              >
-                <Card.Meta
-                  title={work.title}
-                  description={`${work.year} ${work.category}`}
-                />
-              </Card>
-            </Col>
-          ))}
         </Row>
       </Space>
     </Content>
